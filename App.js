81e948dd0d6e9337792c3ca768a07b44659ac2d4
Xis <rick-s@hotmail.com>
2021-04-17 16:58:29 -0700

    added and tested the account screen for layout only NOT functional yet

--- App.js
+++ App.js
@@ -11,15 +11,8 @@ import MessagesScreen from "./app/screens/MessagesScreen";
 import Screen from "./app/components/Screen";
 import AppIcon from "./app/components/AppIcon";
 import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
-  return (
-    <Screen>
-      <ListItem
-        title="My title"
-        // subTitle=" my subtitle"
-        IconComponent={<AppIcon name="email" />}
-      />
-    </Screen>
-  );
+  return <AccountScreen />;
 }
