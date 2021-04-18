53db6733000dcfd22bf3cc190319da570923ac04
Xis <rick-s@hotmail.com>
2021-04-17 21:35:00 -0700

    installed Formik and yup libraries for form generation and validation

--- App.js
+++ App.js
@@ -1,29 +1,12 @@
 import React, { useState } from "react";
 import { View, SafeAreaView, Text, TextInput, Switch } from "react-native";
 
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
+import LoginScreen from "./app/screens/LoginScreen"
 
-const categories = [
-  { label: "Furniture", value: 1 },
-  { label: "Clothes", value: 2 },
-  { label: "Auto", value: 3 },
-];
 
 export default function App() {
-  const [category, setCategory] = useState();
 
   return (
-    <Screen>
-      <AppPicker
-        selectedItem={category}
-        onSelectItem={(item) => setCategory(item)}
-        items={categories}
-        icon="apps"
-        placeholder="Catagory"
-      />
-      <AppTextInput icon="email" placeholder="email" />
-    </Screen>
+    <LoginScreen />
   );
 }
