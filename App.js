b83716bc6f916d549b367fee02f59b0faad51721
Xis <rick-s@hotmail.com>
2021-04-17 17:56:21 -0700

    first text input component completed and tested

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import React from "react";
-import { View, SafeAreaView, Text } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import AppButton from "./app/components/AppButton";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import Screen from "./app/components/Screen";
-import AppIcon from "./app/components/AppIcon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen"
+import React, { useState } from "react";
+import { View, SafeAreaView, Text, TextInput } from "react-native";
 
+import Screen from "./app/components/Screen";
+import AppTextInput from "./app/components/AppTextInput"
 
 export default function App() {
-  return <ListingsScreen />;
+
+
+  return (
+    <Screen>
+      <AppTextInput placeholder="First Name" icon="email"/>
+    </Screen>
+  );
 }
