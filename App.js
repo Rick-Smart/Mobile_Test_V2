20a1bb53e180e0a3c454c6e7d7e8404744d5ffc3
Xis <rick-s@hotmail.com>
2021-04-18 22:53:30 -0700

    added the ability to access the camera roll, select an image save it to state and display to the screen

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import React, { useState } from "react";
-import { View, SafeAreaView, Text, TextInput, Switch } from "react-native";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  TextInput,
+  Switch,
+  Button,
+  Image,
+} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
+import Screen from "./app/components/Screen";
 import MessagesScreen from "./app/screens/MessagesScreen";
 
-
-
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    if (!granted) alert("you need to enable permission to access the library.");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Screen>
+      <Button title="Select" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+    </Screen>
+  );
 }
