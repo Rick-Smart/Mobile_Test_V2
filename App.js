fa5f0233eb264f88be731f238262b14efee0b75f
Xis <rick-s@hotmail.com>
2021-04-19 11:06:00 -0700

    created a FormImagePicker component with a comined ErrorMessage component and added it to our listingEditScreen

--- App.js
+++ App.js
@@ -5,27 +5,14 @@ import * as Permissions from "expo-permissions";
 
 import Screen from "./app/components/Screen";
 import ImageInputList from "./app/components/ImageInputList";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 export default function App() {
-  const [imageUris, setImageUris] = useState([]);
+ 
 
-  const handleAdd = (uri) => {
-    setImageUris([...imageUris, uri]);
-  };
 
-  const handleRemove = (uri) => {
-    setImageUris(imageUris.filter((imageUri) => imageUri !== uri));
-  };
 
   return (
-    <Screen>
-      <View>
-        <ImageInputList
-          imageUris={imageUris}
-          onAddImage={handleAdd}
-          onRemoveImage={handleRemove}
-        />
-      </View>
-    </Screen>
+    <ListingEditScreen />
   );
 }
