ccbe379f3d91e2717fe2f820027db137ba358205
Xis <rick-s@hotmail.com>
2021-04-19 13:14:10 -0700

    installed react-navigation/bottom-tabs

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
@@ -23,15 +24,19 @@ const Tweets = ({ navigation }) => (
 );
 
 const TweetDetails = ({ route }) => (
-    
   <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
+    <Text>Tweet Details</Text>
   </Screen>
 );
 
 const Stack = createStackNavigator();
 const StackNavigator = () => (
-  <Stack.Navigator>
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: "dodgerblue" },
+      headerTintColor: "white",
+    }}
+  >
     <Stack.Screen name="Tweets" component={Tweets} />
     <Stack.Screen
       name="TweetDetails"
@@ -41,10 +46,18 @@ const StackNavigator = () => (
   </Stack.Navigator>
 );
 
+const Tab = createBottomTabNavigator();
+const TabNavigator = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Tweets" component={Tweets} />
+    <Tab.Screen name="TweetDetails" component={TweetDetails} />
+  </Tab.Navigator>
+);
+
 export default function App() {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      <TabNavigator />
     </NavigationContainer>
   );
 }
