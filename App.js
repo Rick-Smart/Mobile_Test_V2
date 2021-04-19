23f7104f0dc7ba076dda6c3d02d060124c9ba9b2
Xis <rick-s@hotmail.com>
2021-04-18 17:27:37 -0700

    created icons for category list

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import { View, SafeAreaView, Text, TextInput, Switch } from "react-native";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 import MessagesScreen from "./app/screens/MessagesScreen";
-import ListItem from "./app/components/ListItem";
-import ListingsScreen from "./app/screens/ListingsScreen";
+
 
 
 export default function App() {
-  return <MessagesScreen />;
+  return <ListingEditScreen />;
 }
--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -18,15 +18,18 @@ export default function AppPicker({
   icon,
   items,
   onSelectItem,
-  selectedItem,
+  numberOfColumns = 1,
+  PickerItemComponent = PickerItem,
   placeholder,
+  selectedItem,
+  width = "100%",
 }) {
   const [modalVisible, setModalVisible] = useState(false);
 
   return (
     <>
       <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
-        <View style={styles.container}>
+        <View style={[styles.container, { width }]}>
           {icon && (
             <MaterialCommunityIcons
               name={icon}
@@ -53,8 +56,10 @@ export default function AppPicker({
           <FlatList
             data={items}
             keyExtractor={(item) => item.value.toString()}
+            numColumns={numberOfColumns}
             renderItem={({ item }) => (
-              <PickerItem
+              <PickerItemComponent
+                item={item}
                 label={item.label}
                 onPress={() => {
                   setModalVisible(false);
@@ -74,7 +79,6 @@ const styles = StyleSheet.create({
     backgroundColor: defaultStyles.colors.light,
     borderRadius: 25,
     flexDirection: "row",
-    width: "100%",
     padding: 15,
     marginVertical: 10,
   },
