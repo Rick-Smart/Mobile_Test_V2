70353cabe1c80513af82e3d42cd4544cce9451c8
Xis <rick-s@hotmail.com>
2021-04-20 16:50:03 -0700

    setup nodeserver and first api call to it to get all of the mock data from the server

---
+++ app/api/client.js
@@ -0,0 +1,5 @@
+import { create } from "apisauce";
+
+export default apiClient = create({
+  baseURL: "http://192.168.0.132:9000/api",
+});
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,27 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, FlatList } from "react-native";
 
 import Card from "../components/Card";
 import colors from "../config/colors";
-import routes from "../navigation/routes"
+import listingsApi from "../api/listings";
+import routes from "../navigation/routes";
 import Screen from "../components/Screen";
 
-const listings = [
-  {
-    id: 1,
-    title: "Red Jacket for Sale",
-    price: 100,
-    image: require("../assets/jacket.jpg"),
-  },
-  {
-    id: 2,
-    title: "chair in great condition",
-    price: 400,
-    image: require("../assets/chair.jpg"),
-  },
-];
-
 export default function ListingsScreen({ navigation }) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
+
   return (
     <Screen style={styles.screen}>
       <FlatList
@@ -31,7 +28,7 @@ export default function ListingsScreen({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
