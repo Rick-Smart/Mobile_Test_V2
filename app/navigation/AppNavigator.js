561677aeb99f5638c7e2dac90fcd205f0246a8e9
Xis <rick-s@hotmail.com>
2021-04-28 12:12:10 -0700

    Completed messaging notifications, will need to update to expo-notifications in the future

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -6,44 +6,52 @@ import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
 import NewListingButon from "./NewListingButon";
+import navigation from "./rootNavigation";
+import useNotifications from "../hooks/useNotifications";
 
 const Tab = createBottomTabNavigator();
 
-export default AppNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen
-      name="Feed"
-      component={FeedNavigator}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="home" color={color} size={size} />
-        ),
-      }}
-    />
-    <Tab.Screen
-      name="ListingEdit"
-      component={ListingEditScreen}
-      options={({ navigation }) => ({
-        tabBarButton: () => (
-          <NewListingButon onPress={() => navigation.navigate("ListingEdit")} />
-        ),
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons
-            name="plus-circle"
-            color={color}
-            size={size}
-          />
-        ),
-      })}
-    />
-    <Tab.Screen
-      name="Account"
-      component={AccountNavigator}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),
-      }}
-    />
-  </Tab.Navigator>
-);
+export default AppNavigator = () => {
+  useNotifications();
+
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Feed"
+        component={FeedNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="ListingEdit"
+        component={ListingEditScreen}
+        options={({ navigation }) => ({
+          tabBarButton: () => (
+            <NewListingButon
+              onPress={() => navigation.navigate("ListingEdit")}
+            />
+          ),
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="plus-circle"
+              color={color}
+              size={size}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="Account"
+        component={AccountNavigator}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
--- app/screens/ListingDetailsScreen.js
+++ app/screens/ListingDetailsScreen.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, KeyboardAvoidingView, Platform } from "react-native";
 import { Image } from "react-native-expo-image-cache";
 
-import AppText from "../components/AppText";
 import colors from "../config/colors";
 import ListItem from "../components/lists/ListItem";
-import Screen from "../components/Screen";
+import AppText from "../components/AppText";
+import SellerFormMessage from "../components/SellerFormMessage";
 
-export default function ListingDetailsScreen({ route }) {
+function ListingDetailsScreen({ route }) {
   const listing = route.params;
 
   return (
-    <Screen>
+    <KeyboardAvoidingView
+      behavior="position"
+      keyboardVerticalOffset={Platform.OS === "ios" ? 0 : 100}
+    >
       <Image
         style={styles.image}
         preview={{ uri: listing.images[0].thumbnailUrl }}
@@ -23,27 +26,24 @@ export default function ListingDetailsScreen({ route }) {
         <AppText style={styles.price}>${listing.price}</AppText>
         <View style={styles.userContainer}>
           <ListItem
-            image={require("../assets/logo-red.png")}
-            title="Rick Smart"
+            image={require("../assets/icon.png")}
+            title="Rick S."
             subTitle="5 Listings"
           />
         </View>
+        <SellerFormMessage listing={listing} />
       </View>
-    </Screen>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
-  image: {
-    width: "100%",
-    height: 300,
-  },
   detailsContainer: {
     padding: 20,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "500",
+  image: {
+    width: "100%",
+    height: 300,
   },
   price: {
     color: colors.secondary,
@@ -51,7 +51,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginVertical: 10,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: "500",
+  },
   userContainer: {
     marginVertical: 40,
   },
 });
+
+export default ListingDetailsScreen;
